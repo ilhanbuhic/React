4827c96d9fee600fa0f52f7ba8547a17b9df62eb
ilhanbuhic <ilhanbuhic@hotmail.com>
2023-09-30 21:02:44 +0200

    Class 3

--- IT Camp/Class 3/src/components/Item.js
+++ IT Camp/Class 3/src/components/Item.js
@@ -1,11 +1,11 @@
 // export default function Item(props) {
 //     return <li>[props.name]</li>
 // }
-export default function Item({name, isBought}) {
-    return (
-        <li>
-        {/* {name} {isBought ? '✅' : '❌'} */}
-        {name} {isBought && '✅'}
-        </li>
-    )
+export default function Item({ name, isBought }) {
+  return (
+    <li>
+      {/* {name} {isBought ? '✅' : '❌'} */}
+      {name} {isBought && "✅"}
+    </li>
+  )
 }
