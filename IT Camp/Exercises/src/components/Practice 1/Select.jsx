5f3922ca84230244764f9363b6ef281843fb0bf1
Ilhan Buhic <ilhanbuhic@hotmail.com>
2023-10-04 19:34:11 +0200

    changes

--- IT Camp/Exercises/src/components/Practice 1/Select.jsx
+++ IT Camp/Exercises/src/components/Practice 1/Select.jsx
@@ -1,12 +1,32 @@
 import { useState } from 'react'
 
 export const Select = ({ data }) => {
+  // const [selectedOption, setSelectedOption] = useState('')
+
+  // const handleSelectChange = (e) => {
+  //     const newValue = e.target.value
+  //     selectedOption(newValue)
+  // }
+
   return (
     <>
       {!data.length ? null : (
         <div className='selectContainer'>
-          <select name='' id=''>
-            <option value='all'>All items</option>
+          <select
+            onChange={(e) => {
+              {
+                console.log(e.target.value)
+              }
+              {
+                data?.map((item) => {
+                  return e.target.value === item.title ? <option value={item.title}>{item.title}</option> : null
+                })
+              }
+            }}
+            name=''
+            id=''
+          >
+             <option value='all'>All items</option>
             {data?.map((item) => {
               return <option value={item.title}>{item.title}</option>
             })}
