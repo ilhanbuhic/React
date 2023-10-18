3b7d2512b26c5ae84df23148979b716ffc706b44
Ilhan Buhic <ilhanbuhic@hotmail.com>
2023-10-18 22:46:47 +0200

    creating single page website

--- IT Camp/Exercises/src/App.js
+++ IT Camp/Exercises/src/App.js
@@ -1,17 +1,24 @@
-import './components/Practice 1/Card.css'
-import './components/Practice 2/css/flex-container.css'
-
 import React from 'react'
+
+// import './components/Practice 1/Card.css'
+// import './components/Practice 2/css/flex-container.css'
+
 // import Card from './components/Practice 1/Card'
 // import { ButtonContainer } from './components/Practice 2/ButtonContainer'
-import { Cards } from './components/Practice 3/Cards'
+// import { Cards } from './components/Practice 3/Cards'
+// ---------- Practice 4 ----------
+import './components/Practice 4/CSS/defaultSettings.css'
+import Hero from './components/Practice 4/Navbar'
+// ---------- Practice 4 ----------
+
 
 
 function App() {
   return <div className='App'>
     {/* <Card /> */}
     {/* <ButtonContainer /> */}
-    <Cards />
+    {/* <Cards /> */}
+    <Hero />
     </div>
 }
 
