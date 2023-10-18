c8597a1932d67f30f105203db6838238db552fcd
Ilhan Buhic <ilhanbuhic@hotmail.com>
2023-10-18 18:30:59 +0200

    RRD useNavigate hook

--- IT Camp/class/src/pages/AboutUsPage.jsx
+++ IT Camp/class/src/pages/AboutUsPage.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 function AboutUsPage() {
+  const navigate = useNavigate()
+
   return (
     <div className='container'>
       <h1>This is ABOUT US page</h1>
+      {/* <Link style={{ color: '#fff' }} to={'/'}>
+        Go to HOME
+      </Link> */}
+      <button onClick={() => navigate('/')}>HOME</button>
     </div>
   )
 }
--- IT Camp/class/src/pages/HomePage.jsx
+++ IT Camp/class/src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function HomePage() {
   return (
     <div className='container'>
       <h1>This is HOME page</h1>
+      <Link style={{ color: '#fff' }} to={'/about'}>
+        ABOUT US
+      </Link>
     </div>
   )
 }
