71233e30b9be29d88722d204cae2ad804ef774ec
Ilhan Buhic <ilhanbuhic@hotmail.com>
2023-11-01 19:35:23 +0100

    newsAPI

--- IT Camp/NewsAPI/src/components/News/Postcards/Postcards.jsx
+++ IT Camp/NewsAPI/src/components/News/Postcards/Postcards.jsx
@@ -1,4 +1,22 @@
-
+import * as React from 'react'
+import { useState, useEffect, useRef } from 'react'
+import { styled } from '@mui/material/styles'
+import Card from '@mui/material/Card'
+import CardHeader from '@mui/material/CardHeader'
+import CardMedia from '@mui/material/CardMedia'
+import CardContent from '@mui/material/CardContent'
+import CardActions from '@mui/material/CardActions'
+import Collapse from '@mui/material/Collapse'
+import Avatar from '@mui/material/Avatar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import { red } from '@mui/material/colors'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import ShareIcon from '@mui/icons-material/Share'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
+import '../Postcards/Postcards.css'
+import axios from 'axios'
 import news_icon from '../../images/news-icon.webp'
 
 const ExpandMore = styled((props) => {
