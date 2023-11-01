eafd44a02b263db33873b0840c962fda4631e699
Ilhan Buhic <ilhanbuhic@hotmail.com>
2023-11-01 19:32:31 +0100

    newsAPI

--- IT Camp/NewsAPI/src/components/News/Postcards/Postcards.jsx
+++ IT Camp/NewsAPI/src/components/News/Postcards/Postcards.jsx
@@ -1,22 +1,4 @@
-import * as React from 'react'
-import { useState, useEffect, useRef } from 'react'
-import { styled } from '@mui/material/styles'
-import Card from '@mui/material/Card'
-import CardHeader from '@mui/material/CardHeader'
-import CardMedia from '@mui/material/CardMedia'
-import CardContent from '@mui/material/CardContent'
-import CardActions from '@mui/material/CardActions'
-import Collapse from '@mui/material/Collapse'
-import Avatar from '@mui/material/Avatar'
-import IconButton from '@mui/material/IconButton'
-import Typography from '@mui/material/Typography'
-import { red } from '@mui/material/colors'
-import FavoriteIcon from '@mui/icons-material/Favorite'
-import ShareIcon from '@mui/icons-material/Share'
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
-import MoreVertIcon from '@mui/icons-material/MoreVert'
-import '../Postcards/Postcards.css'
-import axios from 'axios'
+
 import news_icon from '../../images/news-icon.webp'
 
 const ExpandMore = styled((props) => {
@@ -93,7 +75,61 @@ export default function RecipeReviewCard() {
           data &&
           data?.author !== 'wsj' && (
             <div className='postcard' key={data.id}>
-              
+              <Card sx={{ maxWidth: 345 }}>
+                <CardHeader
+                  avatar={<Avatar src={news_icon} aria-label='recipe'></Avatar>}
+                  action={
+                    <IconButton aria-label='settings'>
+                      <MoreVertIcon />
+                    </IconButton>
+                  }
+                  className='cardHeader'
+                  title={data.author}
+                  subheader={formatDate(data.publishedAt)}
+                />
+                <CardMedia
+                  component='img'
+                  height='194'
+                  image={data.urlToImage}
+                  alt='Paella dish'
+                />
+                <CardContent>
+                  <Typography variant='body2' color='text.secondary'>
+                    <p
+                      dangerouslySetInnerHTML={{
+                        __html: data.content.replace(
+                          /\[\+\d+ chars\]/,
+                          '<span class="read-more">Read more</span>'
+                        ),
+                      }}
+                    ></p>
+                  </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                  <IconButton
+                    ref={iconButtonRef}
+                    onClick={handleClick}
+                    className='like'
+                    aria-label='add to favorites'
+                  >
+                    <FavoriteIcon />
+                  </IconButton>
+                  <IconButton aria-label='share'>
+                    <ShareIcon />
+                  </IconButton>
+                  <ExpandMore
+                    expand={expanded}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    aria-label='show more'
+                  >
+                    <ExpandMoreIcon />
+                  </ExpandMore>
+                </CardActions>
+                <Collapse in={expanded} timeout='auto' unmountOnExit>
+                  <CardContent></CardContent>
+                </Collapse>
+              </Card>
             </div>
           )
       )}
@@ -108,61 +144,7 @@ export default function RecipeReviewCard() {
 //           data?.author !== 'wsj' && (
 //             <div className='postcard' key={data.id}>
 //               <p>{data.author}</p>
-//               <p>{formatDate(data.publishedAt)}</<Card sx={{ maxWidth: 345 }}>
-                <CardHeader
-                avatar={<Avatar src={news_icon} aria-label='recipe'></Avatar>}
-                action={
-                  <IconButton aria-label='settings'>
-                    <MoreVertIcon />
-                  </IconButton>
-                }
-                className='cardHeader'
-                title={data.author}
-                subheader={formatDate(data.publishedAt)}
-              />
-              <CardMedia
-                component='img'
-                height='194'
-                image={data.urlToImage}
-                alt='Paella dish'
-              />
-              <CardContent>
-                <Typography variant='body2' color='text.secondary'>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: data.content.replace(
-                        /\[\+\d+ chars\]/,
-                        '<span class="read-more">Read more</span>'
-                      ),
-                    }}
-                  ></p>
-                </Typography>
-              </CardContent>
-              <CardActions disableSpacing>
-                <IconButton
-                  ref={iconButtonRef}
-                  onClick={handleClick}
-                  className='like'
-                  aria-label='add to favorites'
-                >
-                  <FavoriteIcon />
-                </IconButton>
-                <IconButton aria-label='share'>
-                  <ShareIcon />
-                </IconButton>
-                <ExpandMore
-                  expand={expanded}
-                  onClick={handleExpandClick}
-                  aria-expanded={expanded}
-                  aria-label='show more'
-                >
-                  <ExpandMoreIcon />
-                </ExpandMore>
-              </CardActions>
-              <Collapse in={expanded} timeout='auto' unmountOnExit>
-                <CardContent></CardContent>
-              </Collapse>
-            </Card>p>
+//               <p>{formatDate(data.publishedAt)}</p>
 //               <p>{data.content.replace(/\[\+\d+ chars\]/, 'Read more')}</p>
 //             </div>
 //           )
