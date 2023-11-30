c2b6e2ce0e950478130a2cd2f7c66af4ae93aa3c
Ilhan Buhic <ilhanbuhic@hotmail.com>
2023-11-30 23:16:50 +0100

    .

--- IT Camp/omdbAPI/src/pages/Movies.tsx
+++ IT Camp/omdbAPI/src/pages/Movies.tsx
@@ -28,7 +28,29 @@ function Movies() {
   }
 
   return (
-
+    <div className='w-full h-[550px] text-white'>
+      <div className='w-full h-full'>
+        <div className='absolute w-full h-[550px] bg-gradient-to-r from-black'></div>
+        <img
+          className='w-full h-full object-cover object-top'
+          src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+          alt={movie?.title}
+        />
+        <div className='absolute w-full top-[20%] p-4 md:p-8'>
+          <h1 className='text-3xl md: text-5xl font-bold'>{movie?.title}</h1>
+          <div className='my-4'>
+            <button className='border bg-gray-300 text-black border-gray-300 py-4 px-8 text-[1.2rem]'>
+              Play
+            </button>
+            <button className='border  text-white border-gray-300 py-4 px-8 ml-4 text-[1.2rem]'>
+              Watch Later
+            </button>
+          </div>
+          <p className='text-xl text-gray-400'>Released: {movie?.release_date}</p>
+          <p className='w-full text-[1.7rem] mt-10 md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200'>{truncateString(movie?.overview, 150)}</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
