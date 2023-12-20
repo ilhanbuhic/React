b90a14dd3483b06d9477a3ac57828d13c3829c01
Ilhan Buhic <ilhanbuhic@hotmail.com>
2023-12-20 22:28:50 +0100

    updating savedshows.tsx

--- IT Camp/omdbAPI/src/components/SavedShows.tsx
+++ IT Camp/omdbAPI/src/components/SavedShows.tsx
@@ -31,7 +31,43 @@ const SavedShows = () => {
     
   return (
     <div>
-     
+      <h2 className='text-white text-[16px] bg-black font-bold md:text-[20px] p-4'>
+        My Shows
+      </h2>
+      <div className='relative bg-black flex items-center justify-center group'>
+        <MdChevronLeft
+          onClick={slideLeft}
+          className='bg-white left-0 text-black rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block '
+          size={40}
+        />
+        <div
+          id={'slider'}
+          className='bar w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth relative '
+        >
+          {movies.map((item: any, id: number) => (
+            <div
+              key={id}
+              className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'
+            >
+              <img
+                className='w-full h-auto block'
+                src={`https://image.tmdb.org/t/p/original/${item?.img}`}
+                alt={item?.title}
+              />
+              <div className='absolute top-0 left-0 w-full h-full hover:bg-black/60 opacity-0 hover:opacity-100  text-white'>
+                <p className='whitespace-normal max-w-full text-[10px] tracking-[1.2px] font-[500] md:text-[15px] flex justify-center items-center h-full text-center'>
+                  {item?.title}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <MdChevronRight
+          onClick={slideRight}
+          className='bg-white right-12 text-black rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
+          size={40}
+        />
+      </div>
     </div>
   )
 }
