fc51024e5d8dbf6eb7c5678d6f09052b710b5bd0
Ilhan Buhic <ilhanbuhic@hotmail.com>
2024-04-16 18:56:19 +0200

    .

--- IT Camp/trackmyruns/src/components/SidebarForm.tsx
+++ IT Camp/trackmyruns/src/components/SidebarForm.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SidebarFormProp } from '../utils/interface'
+
+// const [hidden, setHidden] = useState<string>('none')
 
 export const SidebarForm: React.FC<SidebarFormProp> = ({
   className,
@@ -9,7 +11,7 @@ export const SidebarForm: React.FC<SidebarFormProp> = ({
   return (
     <>
       <div className={`form__row ${className}`}>
-        <label className={`form__label ${className}`}>{labelName}</label>
+        <label className={`form__label`}>{labelName}</label>
         <input
           className={`form__input form__input--${className}`}
           placeholder={inputPlaceholder}
