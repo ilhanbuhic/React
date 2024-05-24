9e9c00adf214c9ac14048e385bb838039217a76e
Ilhan Buhic <ilhanbuhic@hotmail.com>
2024-05-24 17:04:04 +0200

    fixing the map, adding leaflet/dist/leaflet.css

--- IT Camp/trackmyruns/src/components/Sidebar.tsx
+++ IT Camp/trackmyruns/src/components/Sidebar.tsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { SidebarForm } from './SidebarForm'
 import { logo } from '../assets/images/index'
-import L, { map } from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+import L, { latLng } from 'leaflet'
+
+import 'leaflet/dist/leaflet.css'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const Sidebar = () => {
   const months = [
@@ -20,34 +24,33 @@ const Sidebar = () => {
   ]
 
   const [activeCandence, setActiveCandence] = useState('')
+  const [coords, setCoords] = useState<[number, number] | null>(null)
 
   useEffect(() => {
-  if (navigator.geolocation)
-    navigator.geolocation.getCurrentPosition(
-      function (position) {
-        const { latitude } = position.coords
-        const { longitude } = position.coords
-        console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
-        const coords = L.latLng(latitude, longitude)
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        function (position) {
+          const { latitude, longitude } = position.coords
+          // const { longitude } = position.coords
+          // console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
 
-        // if (!L.DomUtil.get('map')) {
-        const map = L.map('map').setView(coords, 13)
+          setCoords([latitude, longitude])
+          // const map = L.map('map').setView(coords, 15)
 
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          attribution:
-            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        }).addTo(map)
+          // L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          //   attribution:
+          //     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+          // }).addTo(map)
 
-        L.marker(coords)
-          .addTo(map)
-          .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-          .openPopup()
-        // }
-      },
-      function () {
-        alert('Could not get your position')
-      }
-    )
+          // L.marker(coords)
+          //   .addTo(map)
+          //   .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+          //   .openPopup()
+        },
+        function () {
+          alert('Could not get your position')
+        }
+      )
   }, [])
 
   return (
@@ -189,7 +192,25 @@ const Sidebar = () => {
           as your own.
         </p>
       </div>
-      <div id='map'></div>
+      {coords && (
+        <MapContainer
+          id='map'
+          center={coords}
+          zoom={14}
+          style={{ height: '100vh', width: '100%' }}
+        >
+          <TileLayer
+            url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+
+          <Marker position={coords}>
+            <Popup>
+              A pretty CSS popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      )}
     </div>
   )
 }
