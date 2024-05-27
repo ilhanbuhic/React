e486deebdfe2178a5a301ddd4acd2c4600760014
Ilhan Buhic <ilhanbuhic@hotmail.com>
2024-05-27 18:38:20 +0200

    adding styles to popup, creating .on method with map, making custom icon(default not working)

--- IT Camp/trackmyruns/src/assets/images/index.ts
+++ IT Camp/trackmyruns/src/assets/images/index.ts
@@ -1,3 +1,17 @@
+import L from 'leaflet'
+
 import logo from '../images/logo.png'
 
-export { logo }
+const marker = L.icon({
+  iconUrl:
+    'https://www.pikpng.com/pngl/m/509-5093828_map-marker-car-icon-free-icon-download-svg.png',
+  //   iconSize: [38, 95],
+  iconSize: [35, 55],
+  iconAnchor: [22, 94],
+  popupAnchor: [-3, -76],
+  shadowUrl: '/',
+  shadowSize: [35, 55],
+  shadowAnchor: [22, 94],
+})
+
+export { logo, marker }
--- IT Camp/trackmyruns/src/components/Map.tsx
+++ IT Camp/trackmyruns/src/components/Map.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import 'leaflet/dist/leaflet.css'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import L, { latLng } from 'leaflet'
+import { marker } from '../assets/images'
 
 export const Map: React.FC = () => {
   const [coords, setCoords] = useState<[number, number] | null>(null)
@@ -10,48 +12,47 @@ export const Map: React.FC = () => {
       navigator.geolocation.getCurrentPosition(
         function (position) {
           const { latitude, longitude } = position.coords
-          // const { longitude } = position.coords
+          console.log(latitude, longitude)
+
           // console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
 
-          setCoords([latitude, longitude])
-          // const map = L.map('map').setView(coords, 15)
+          const coords = latLng(latitude, longitude)
 
-          // L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          //   attribution:
-          //     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-          // }).addTo(map)
+          const map = L.map('map').setView(coords, 15)
+          console.log(map)
 
-          // L.marker(coords)
-          //   .addTo(map)
-          //   .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-          //   .openPopup()
+          L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution:
+              '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+          }).addTo(map)
+
+          map.on('click', function (mapEvent) {
+            const { lat, lng } = mapEvent.latlng
+
+            L.marker([lat, lng], { icon: marker })
+              .addTo(map)
+              .bindPopup(
+                L.popup({
+                  maxWidth: 250,
+                  minWidth: 100,
+                  autoClose: false,
+                  closeOnClick: false,
+                  className: 'running-popup',
+                })
+              )
+              .setPopupContent('Workout')
+              .openPopup()
+          })
         },
         function () {
           alert('Could not get your position')
         }
       )
   }, [])
+
   return (
     <>
-      {coords && (
-        <MapContainer
-          id='map'
-          center={coords}
-          zoom={14}
-          style={{ height: '100vh', width: '100%' }}
-        >
-          <TileLayer
-            url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          />
-
-          <Marker position={coords}>
-            <Popup>
-              A pretty CSS popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer>
-      )}
+      <div id='map'></div>
     </>
   )
 }
