85c9db13d875b4b992db186c9f42eb0a2f5f8afb
Ilhan Buhic <ilhanbuhic@hotmail.com>
2024-09-07 03:24:41 +0200

    add sort feature

--- TravelList/src/components/PackingList/PackingList.tsx
+++ TravelList/src/components/PackingList/PackingList.tsx
@@ -18,10 +18,24 @@ const PackingList: React.FC<PackingListProp> = ({
   onDeleteItem,
   onTogglePacked,
 }) => {
+  const [sortBy, setSortBy] = useState('input')
+
+  let sortedItems
+
+  if (sortBy === 'input') sortedItems = items
+  if (sortBy === 'description')
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description))
+  if (sortBy === 'packed')
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed))
+
   return (
     <div className='list'>
       <ul>
-        {items.map((item) => (
+        {sortedItems?.map((item) => (
           <ItemComponent
             key={item.id}
             item={item}
@@ -30,6 +44,13 @@ const PackingList: React.FC<PackingListProp> = ({
           />
         ))}
       </ul>
+      <div className='action'>
+        <select onChange={(e) => setSortBy(e.target.value)}>
+          <option value='input'>Sort by input order</option>
+          <option value='description'>Sort by description</option>
+          <option value='packed'>Sort by packed status</option>
+        </select>
+      </div>
     </div>
   )
 }
